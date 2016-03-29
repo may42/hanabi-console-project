e92e62d74c975b2068936a8f12ff20de02e55f64
may42 <qozquz@gmail.com>
2016-03-29 21:35:28 +0500

    init project
    initialized my console project

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq;
+
+namespace my_console_project
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadKey();
+        }
+    }
+}
